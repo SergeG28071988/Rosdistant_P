4e0d4b784a125ad4b2d7b3a4c99b4cc8c5e11724
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-04-06 11:53:22 +0300

    A model has been created and migrations have been performed

--- autoweb/auto_app/admin.py
+++ autoweb/auto_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Auto
+
 
 # Register your models here.
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'color')
--- autoweb/auto_app/models.py
+++ autoweb/auto_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Auto(models.Model):
+    brand = models.CharField(max_length=100,
+                             help_text="Введите название бренда",
+                             verbose_name="Бренд")
+    model = models.CharField(max_length=100,
+                             help_text="Введите модель",
+                             verbose_name="Модель")
+    year = models.IntegerField(help_text="Введите год выпуска", null=True, blank=True)
+    color = models.CharField(max_length=50,
+                             help_text="Введите цвет автомобиля",
+                             verbose_name="Цвет автомобиля")
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year}) - {self.color}"
+
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
